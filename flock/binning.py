Finding best balance between UMAP contrasting for multi and single samples runs. Fixed int overflow error for lorikeet

flock/binning.py
@@ -196,7 +196,7 @@ class Binner():
             assembly,
             scaler="clr",
             n_neighbors=100,
-            min_dist=0.0,
+            min_dist=0.1,
             n_components=2,
             random_state=42,
             min_cluster_size=100,
@@ -206,7 +206,7 @@ class Binner():
             cluster_selection_method="eom",
             precomputed=False,
             hdbscan_metric="euclidean",
-            metric = 'aggregate_variant_tnf',
+            metric = 'aggregate_tnf',
             threads=8,
     ):
         self.threads = threads
@@ -262,35 +262,44 @@ class Binner():
                                                 .iloc[:, 1:].astype(np.float64) + 1)
 
         if self.n_samples < 3:
-            # self.depths = self.depths.values.reshape(1, -1)
-            # self.variances = self.variances.values.reshape(1, -1)
+            # RobustScaler requires rows as samples
+            # self.depths = self.depths.values.T.reshape(1, -1)
+            # self.variance = self.variance.values.T.reshape(1, -1)
             # self.snv_rates = self.snv_rates.values.reshape(1, -1)
-            # self.depths = skbio.stats.composition.clr(self.depths.T.astype(np.float64) + 1).T
-
-            self.depths = np.nan_to_num(RobustScaler().fit_transform(self.depths), nan=0.0, posinf=0.0, neginf=0.0)
-            self.variance = RobustScaler().fit_transform(np.nan_to_num(self.variance, nan=0.0, posinf=0.0, neginf=0.0))
+            self.depths = skbio.stats.composition.clr(self.depths.T.astype(np.float64) + 1).T
+            self.variance = skbio.stats.composition.clr(self.variance.T.astype(np.float64) + 1).T
+            if self.n_samples == 1:
+                self.depths = self.depths[:, None]
+                # self.variance = RobustScaler().fit_transform(np.nan_to_num(self.variance.T, nan=0.0, posinf=0.0, neginf=0.0)).T
+            # self.tnf = np.nan_to_num(np.concatenate((self.large_contigs.iloc[:, 1].values[:, None], self.tnfs), axis=1))
             self.depths = np.nan_to_num(np.concatenate((self.large_contigs.iloc[:, 1].values[:, None], self.depths, self.tnfs), axis=1))
-
+# 
             # self.snv_rates = RobustScaler().fit_transform(np.nan_to_num(self.snv_rates, nan=0.0, posinf=0.0, neginf=0.0))
             # self.variance = RobustScaler().fit_transform(np.nan_to_num(self.variance, nan=0.0, posinf=0.0, neginf=0.0))
         else:
             self.depths = skbio.stats.composition.clr(self.depths.T.astype(np.float64) + 1).T
+            # self.depths = np.nan_to_num(np.concatenate((self.large_contigs.iloc[:, 1].values[:, None], self.depths, self.tnfs), axis=1))
+            self.tnf = np.nan_to_num(np.concatenate((self.large_contigs.iloc[:, 1].values[:, None], self.tnfs), axis=1))
             # self.snv_rates = RobustScaler().fit_transform(np.nan_to_num(self.snv_rates, nan=0.0, posinf=0.0, neginf=0.0))
-            self.variance = RobustScaler().fit_transform(np.nan_to_num(self.variance, nan=0.0, posinf=0.0, neginf=0.0))
+            # self.variance = RobustScaler().fit_transform(np.nan_to_num(self.variance, nan=0.0, posinf=0.0, neginf=0.0))
 
 
         
         if self.n_samples >= 3:
             # Three UMAP reducers for each input type
             self.tnf_reducer = umap.UMAP(
-                metric=metrics.rho,
+                metric=metrics.rho_tnf,
                 metric_kwds={"n_samples": self.n_samples},
                 n_neighbors=n_neighbors,
                 n_components=n_components,
                 min_dist=0,
                 random_state=random_state,
-                spread=1,
+                n_epochs=500,
+                # spread=0.5,
+                a=1.58,
+                b=0.5,
             )
+            
             self.depth_reducer = umap.UMAP(
                 metric=metrics.rho,
                 metric_kwds={"n_samples": self.n_samples},
@@ -298,7 +307,10 @@ class Binner():
                 n_components=n_components,
                 min_dist=min_dist,
                 random_state=random_state,
-                spread=1,
+                n_epochs=500,
+                # spread=0.5,
+                a=1.58,
+                b=0.5,
             )
             
             self.variance_reducer = umap.UMAP(
@@ -307,10 +319,26 @@ class Binner():
                 n_components=n_components,
                 min_dist=min_dist,
                 random_state=random_state,
-                spread=1,
+                n_epochs=500,
+                spread=0.5,
+                a=5,
+                b=0.25,
             )
             
         else:
+            # self.tnf_reducer = umap.UMAP(
+                # metric=metrics.rho_tnf,
+                # metric_kwds={"n_samples": self.n_samples},
+                # n_neighbors=n_neighbors,
+                # n_components=n_components,
+                # min_dist=0,
+                # random_state=random_state,
+                # n_epochs=500,
+                # spread=0.5,
+                # a=5,
+                # b=0.25,
+            # )
+            
             self.depth_reducer = umap.UMAP(
                 metric=metrics.aggregate_tnf,
                 metric_kwds={"n_samples": self.n_samples},
@@ -318,7 +346,10 @@ class Binner():
                 n_components=n_components,
                 min_dist=min_dist,
                 random_state=random_state,
-                spread=1,
+                n_epochs=500,
+                spread=0.5,
+                a=1.58,
+                b=0.1,
             )
 
 
@@ -338,23 +369,29 @@ class Binner():
         # snv_mapping = self.snv_reducer.fit(self.snv_rates)
         
         logging.info("Running UMAP - %s" % self.depth_reducer)
-        depth_mapping = self.depth_reducer.fit(self.depths)
+        try:
+            depth_mapping = self.depth_reducer.fit(self.depths)
+        except ValueError: # Sparse or low coverage contigs can cause high n_neighbour values to kark it
+            self.depth_reducer.n_neighbors = 30
+            depth_mapping = self.depth_reducer.fit(self.depths)
         
         if self.n_samples >=3:
             logging.info("Running UMAP - %s" % self.tnf_reducer)
             tnf_mapping = self.tnf_reducer.fit(self.tnfs)
-            logging.info("Running UMAP - %s" % self.variance_reducer)
-            variance_mapping = self.variance_reducer.fit(self.variance)
+            # logging.info("Running UMAP - %s" % self.variance_reducer)
+            # variance_mapping = self.variance_reducer.fit(self.variance)
             ## Contrast all reducers
-            contrast_mapper = (tnf_mapping - variance_mapping) + depth_mapping
+            contrast_mapper = depth_mapping - (tnf_mapping) #- (variance_mapping)
         else:
-            contrast_mapper = depth_mapping #- (depth_mapping - variance_mapping)
+            # logging.info("Running UMAP - %s" % self.tnf_reducer)
+            # tnf_mapping = self.tnf_reducer.fit(self.tnfs)
+            contrast_mapper = depth_mapping #* (tnf_mapping) #- variance_mapping)
         self.embeddings = contrast_mapper.embedding_
 
     def cluster(self):
         ## Cluster on the UMAP embeddings and return soft clusters
         logging.info("Running HDBSCAN")
-        tuned = utils.hyperparameter_selection(self.embeddings, self.threads)
+        tuned = utils.hyperparameter_selection(self.embeddings, self.threads, allow_single_cluster=True)
         best = utils.best_validity(tuned)
         self.clusterer = hdbscan.HDBSCAN(
             algorithm='best',
@@ -366,7 +403,7 @@ class Binner():
             metric='euclidean',
             min_cluster_size=int(best['min_cluster_size']),
             min_samples=int(best['min_samples']),
-            allow_single_cluster=False,
+            allow_single_cluster=True,
             core_dist_n_jobs=self.threads,
             prediction_data=True
         )
@@ -375,7 +412,7 @@ class Binner():
             self.validity, self.cluster_validity = hdbscan.validity.validity_index(self.embeddings.astype(np.float64), self.clusterer.labels_, per_cluster_scores=True)
         except ValueError:
             self.validity = 0
-            self.cluster_validity = [0.5 for i in range(len(set(self.clusterer.labels_)))]
+            self.cluster_validity = [0.0 for i in range(len(set(self.clusterer.labels_)))]
         self.soft_clusters = hdbscan.all_points_membership_vectors(
             self.clusterer)
         self.soft_clusters_capped = np.array([np.argmax(x) for x in self.soft_clusters])
@@ -405,11 +442,11 @@ class Binner():
     def plot(self):
         logging.info("Generating UMAP plot with labels")
 
-        # label_set = set(self.clusterer.labels_)
-        # color_palette = sns.color_palette('Paired', len(label_set))
-        # cluster_colors = [
-            # color_palette[x] if x >= 0 else (0.5, 0.5, 0.5) for x in self.clusterer.labels_ if x != -1
-        # ]
+        label_set = set(self.clusterer.labels_)
+        color_palette = sns.color_palette('Paired', max(self.clusterer.labels_) + 1)
+        cluster_colors = [
+            color_palette[x] if x >= 0 else (0.0, 0.0, 0.0) for x in self.clusterer.labels_
+        ]
 # 
         # cluster_member_colors = [
             # sns.desaturate(x, p) for x, p in zip(cluster_colors, self.clusterer.probabilities_)
@@ -422,7 +459,7 @@ class Binner():
                    self.embeddings[:, 1],
                    s=7,
                    linewidth=0,
-                   c=self.clusterer.labels_,
+                   c=cluster_colors,
                    alpha=0.7)
 
         plt.gca().set_aspect('equal', 'datalim')
@@ -455,22 +492,22 @@ class Binner():
         if len(set(self.clusterer.labels_)) > 5:
             for (idx, label) in enumerate(self.clusterer.labels_):
                 if label != -1:
-                    if self.cluster_validity[label] >= 0.5:
-                        try:
-                            self.bins[label.item() + 1].append(
-                                self.large_contigs.iloc[idx, 0:2].name.item()) # inputs values as tid
-                        except KeyError:
-                            self.bins[label.item() + 1] = [self.large_contigs.iloc[idx, 0:2].name.item()]
-                    else:
-                        try:
-                            redo_bins[label.item() + 1]["size"] += self.large_contigs.iloc[idx, 1]
-                            redo_bins[label.item() + 1]["embeddings"].append(self.embeddings[idx, :])
-                            redo_bins[label.item() + 1]["indices"].append(idx)
-                        except KeyError:
-                            redo_bins[label.item() + 1] = {}
-                            redo_bins[label.item() + 1]["size"] = self.large_contigs.iloc[idx, 1]
-                            redo_bins[label.item() + 1]["embeddings"] = [self.embeddings[idx, :]]
-                            redo_bins[label.item() + 1]["indices"] = [idx]
+                    # if self.cluster_validity[label] >= 0.0:
+                    try:
+                        self.bins[label.item() + 1].append(
+                            self.large_contigs.iloc[idx, 0:2].name.item()) # inputs values as tid
+                    except KeyError:
+                        self.bins[label.item() + 1] = [self.large_contigs.iloc[idx, 0:2].name.item()]
+                    # else:
+                        # try:
+                            # redo_bins[label.item() + 1]["size"] += self.large_contigs.iloc[idx, 1]
+                            # redo_bins[label.item() + 1]["embeddings"].append(self.embeddings[idx, :])
+                            # redo_bins[label.item() + 1]["indices"].append(idx)
+                        # except KeyError:
+                            # redo_bins[label.item() + 1] = {}
+                            # redo_bins[label.item() + 1]["size"] = self.large_contigs.iloc[idx, 1]
+                            # redo_bins[label.item() + 1]["embeddings"] = [self.embeddings[idx, :]]
+                            # redo_bins[label.item() + 1]["indices"] = [idx]
                         
                 else:
                     self.unbinned_indices.append(idx)
@@ -497,8 +534,8 @@ class Binner():
                 for (idx, label) in zip(values["indices"], new_labels):
                     # Update labels
                     if label != -1:
-                        self.clusterer.labels_[idx] = label + max_bin_id - 1
-                        self.soft_clusters_capped[idx] = label + max_bin_id - 1
+                        self.clusterer.labels_[idx] = label.item() + max_bin_id
+                        self.soft_clusters_capped[idx] = label.item() + max_bin_id
                         try:
                             self.bins[label.item() + max_bin_id].append(
                                 self.large_contigs.iloc[idx, 0:2].name.item())  # inputs values as tid
@@ -508,18 +545,22 @@ class Binner():
                         self.unbinned_indices.append(idx)
                         self.unbinned_embeddings.append(self.embeddings[idx, :])
 
+        removed_bins = redo_bins.keys()
+        if len(removed_bins) > 0:
+            self.clusterer.labels_[:] = [label - sum(i < label for i in removed_bins) if label not in removed_bins else label for label in self.clusterer.labels_]
+            
         # break up very large bins. Not sure how to threshold this
         for (bin, values) in redo_bins.items():
             new_labels = utils.break_overclustered(np.array(values["embeddings"]), self.threads)
             try:
-                max_bin_id = max(self.bins.keys()) + 1
+                max_bin_id = max([label.item() for label in set(self.clusterer.labels_) if label not in removed_bins]) + 1
             except ValueError:
                 max_bin_id = 1
             for (idx, label) in zip(values["indices"], new_labels):
                 if label != -1:
                     # Update labels
-                    self.clusterer.labels_[idx] = label + max_bin_id - 1
-                    self.soft_clusters_capped[idx] = label + max_bin_id - 1
+                    self.clusterer.labels_[idx] = label.item() + max_bin_id
+                    self.soft_clusters_capped[idx] = label.item() + max_bin_id
                     try:
                         self.bins[label.item() + max_bin_id].append(
                             self.large_contigs.iloc[idx, 0:2].name.item())  # inputs values as tid
@@ -550,12 +591,6 @@ class Binner():
                         self.large_contigs.iloc[idx, 0:2].name.item()) # inputs values as tid
                 except KeyError:
                     self.bins[label.item() + max_bin_id] = [self.large_contigs.iloc[idx, 0:2].name.item()]
-            # elif self.n_samples < 3:
-                # soft_label = self.soft_clusters_capped[idx]
-                # try:
-                    # self.bins[soft_label.item() + 1].append(self.large_contigs.iloc[idx, 0:2].name.item())
-                # except KeyError:
-                    # self.bins[soft_label.item() + 1] = [self.large_contigs.iloc[idx, 0:2].name.item()]
             else:
                 ## bin out the unbinned contigs again as label 0. Rosella will try to rescue them if there
                 ## are enough samples

flock/cluster.py
@@ -230,9 +230,13 @@ class Cluster():
         )
         logging.info("Running HDBSCAN - %s" % self.clusterer)
         self.clusterer.fit(self.embeddings)
-        self.validity, self.cluster_validity = hdbscan.validity.validity_index(self.embeddings.astype(np.float64),
-                                                                               self.clusterer.labels_,
-                                                                               per_cluster_scores=True)
+        try:
+            self.validity, self.cluster_validity = hdbscan.validity.validity_index(self.embeddings.astype(np.float64),
+                                                                                   self.clusterer.labels_,
+                                                                                   per_cluster_scores=True)
+        except ValueError:
+            self.validity = None
+            self.cluster_validity = [0.5 for i in range(len(set(self.clusterer.labels_)))]
         self.soft_clusters = hdbscan.all_points_membership_vectors(
             self.clusterer)
         self.soft_clusters_capped = np.array([np.argmax(x) for x in self.soft_clusters])
@@ -264,7 +268,7 @@ class Cluster():
         self.clusterer.fit(self.embeddings)
 
     def plot(self):
-        color_palette = sns.color_palette('Paired', 200)
+        color_palette = sns.color_palette('Paired', max(self.clusterer.labels_) + 1)
         cluster_colors = [
             color_palette[x] if x >= 0 else (0.5, 0.5, 0.5) for x in self.clusterer.labels_
         ]
@@ -287,35 +291,38 @@ class Cluster():
     def plot_distances(self):
         self.clusterer.condensed_tree_.plot(
             select_clusters=True,
-            selection_palette=sns.color_palette('deep', 20))
+            selection_palette=sns.color_palette('deep', len(set(self.clusterer.labels_))))
         plt.title('Hierarchical tree of clusters', fontsize=24)
         plt.savefig(self.path + '_UMAP_projection_with_clusters.png')
 
     def labels(self):
         try:
-            return self.soft_clusters_capped.astype('int8')
+            return self.soft_clusters_capped.astype('int32')
         except AttributeError:
-            return self.clusterer.labels_.astype('int8')
+            return self.clusterer.labels_.astype('int32')
 
     def break_clusters(self):
         redo_bins = {}
 
         for (idx, label) in enumerate(self.clusterer.labels_):
             if label != -1:
-                if self.cluster_validity[label] <= 0.5:
+                if self.cluster_validity[label] < 0.0:
                     try:
-                        redo_bins[label.item() + 1]["embeddings"].append(self.embeddings[idx, :])
-                        redo_bins[label.item() + 1]["indices"].append(idx)
+                        redo_bins[label.item()]["embeddings"].append(self.embeddings[idx, :])
+                        redo_bins[label.item()]["indices"].append(idx)
                     except KeyError:
-                        redo_bins[label.item() + 1] = {}
-                        redo_bins[label.item() + 1]["embeddings"] = [self.embeddings[idx, :]]
-                        redo_bins[label.item() + 1]["indices"] = [idx]
+                        redo_bins[label.item()] = {}
+                        redo_bins[label.item()]["embeddings"] = [self.embeddings[idx, :]]
+                        redo_bins[label.item()]["indices"] = [idx]
 
+        removed_labels = redo_bins.keys()
+        self.clusterer.labels_[:] = [label - sum(i < label for i in removed_labels) if label not in removed_labels else label for label in self.clusterer.labels_]
+        
+        
         # break up very large bins. Not sure how to threshold this
-        max_bin_id = max(set(self.clusterer.labels_))
+        max_bin_id = max([label for label in set(self.clusterer.labels_) if label not in removed_labels]) + 1
         for (bin, values) in redo_bins.items():
             new_labels = utils.break_overclustered(np.array(values["embeddings"]), self.threads)
-            max_bin_id = max(set(self.clusterer.labels_))
             for (idx, label) in zip(values["indices"], new_labels):
                 if label != -1:
                     # Update labels

flock/metrics.py
@@ -46,16 +46,16 @@ import math
 @numba.njit()
 def tnf(a, b, n_samples):
     # L2 norm is equivalent to euclidean distance
-    # cov_mat = np.cov(a[n_samples:], b[n_samples:])
-    # cov = cov_mat[0, 1]
-    # a_sd = np.sqrt(cov_mat[0,0])
-    # b_sd = np.sqrt(cov_mat[1,1])
-    # rho = cov / (a_sd * b_sd)
-    # rho += 1
-    # rho = 2 - rho
-    # return rho
-    euc_dist = np.linalg.norm(a[n_samples:] - b[n_samples:])
-    return euc_dist
+    cov_mat = np.cov(a[n_samples:], b[n_samples:])
+    cov = cov_mat[0, 1]
+    a_sd = np.sqrt(cov_mat[0,0])
+    b_sd = np.sqrt(cov_mat[1,1])
+    rho = cov / (a_sd * b_sd)
+    rho += 1
+    rho = 2 - rho
+    return rho
+    # euc_dist = np.linalg.norm(a[n_samples:] - b[n_samples:])
+    # return euc_dist
 
 @numba.njit()
 def snv_corr(a, b, n_samples):
@@ -96,7 +96,8 @@ def sv_corr(a, b, n_samples):
 def euclidean(a, b, n_samples):
     # Since these compositonal arrays are CLR transformed
     # This is the equivalent to the aitchinson distance but we calculat the l2 norm
-    euc_dist = np.linalg.norm(a[:n_samples*2] - b[:n_samples*2])
+    euc_dist = np.linalg.norm(a[:n_samples] - b[:n_samples])
+
     return euc_dist
 
 @numba.njit()
@@ -129,7 +130,7 @@ def sv_euclidean(a, b, n_samples):
 def rho(a, b, n_samples):
     # This is a transformed, inversed version of rho. Normal those -1 <= rho <= 1
     # transformed rho: 0 <= rho <= 2, where 0 is perfect concordance
-    covariance_mat = np.cov(a, b, rowvar=True)
+    covariance_mat = np.cov(a[:n_samples], b[:n_samples], rowvar=True)
     covariance = covariance_mat[0, 1]
     var_a = covariance_mat[0, 0]
     var_b = covariance_mat[1, 1]
@@ -137,6 +138,25 @@ def rho(a, b, n_samples):
     rho = 1 - vlr / (var_a + var_b)
     rho += 1
     rho = 2 - rho
+    
+    return rho
+
+
+@numba.njit()
+def rho_tnf(a, b, n_samples):
+    # This is a transformed, inversed version of rho. Normal those -1 <= rho <= 1
+    # transformed rho: 0 <= rho <= 2, where 0 is perfect concordance
+    w = n_samples / (n_samples + 1) # weighting by number of samples same as in metabat2
+    l = min(a[0], b[0]) / (max(a[0], b[0]) + 1)
+    
+    covariance_mat = np.cov(a[1:n_samples], b[1:n_samples], rowvar=True)
+    covariance = covariance_mat[0, 1]
+    var_a = covariance_mat[0, 0]
+    var_b = covariance_mat[1, 1]
+    vlr = -2 * covariance + var_a + var_b
+    rho = 1 - vlr / (var_a + var_b)
+    rho += 1
+    rho = (2 - rho)
     
     return rho
 
@@ -176,10 +196,17 @@ def phi_dist(a, b, n_samples):
 
 @numba.njit()
 def aggregate_tnf(a, b, n_samples):
-    tnf_dist = tnf(a, b, n_samples)
-    aitchinson = euclidean(a[1:], b[1:], n_samples)
+    w = n_samples / (n_samples + 1) # weighting by number of samples same as in metabat2
+    l = min(a[0], b[0]) / (max(a[0], b[0]) + 1)
+
+    tnf_dist = tnf(a[1:], b[1:], n_samples)
+    if n_samples < 3:  
+        aitchinson = euclidean(a[1:], b[1:], n_samples)  
+        agg = (tnf_dist) * (aitchinson)
+    else:
+        rho_d = rho(a[1:], b[1:], n_samples)
+        agg = (tnf_dist) * rho_d
     
-    agg = (tnf_dist) * (aitchinson)
 
     return agg
 
@@ -195,12 +222,12 @@ def aggregate_variant_tnf(a, b, n_samples):
     if n_samples >= 3:
         snv = snv_corr(a[1:], b[1:], n_samples)
         sv = sv_corr(a[1:], b[1:], n_samples)
-        agg = (tnf_dist ** ((1 - w) * l)) * (aitchinson) * snv * sv
+        agg = (tnf_dist) * (aitchinson) * snv * sv
         return agg
     else:
 
         # Weighted average of these metrics. Weighted by w
-        agg = (tnf_dist ** ((1 - w) * l)) * (aitchinson)
+        agg = (tnf_dist) * (aitchinson)
 
         return agg
 
@@ -210,7 +237,7 @@ def tnf_dist(a, b, n_samples):
     # Need to weigh by differences in contig size. TNF becomes less reliable as contigs diverge in size
     # l = min(a[0], b[0]) / (max(a[0], b[0]) + 1)
 
-    covariance_mat = np.cov(a[1+n_samples*2:], b[1+n_samples*2:], rowvar=True)
+    covariance_mat = np.cov(a[1+n_samples:], b[1+n_samples:], rowvar=True)
     covariance = covariance_mat[0, 1]
     var_a = covariance_mat[0, 0]
     var_b = covariance_mat[1, 1]

flock/utils.py
@@ -39,12 +39,13 @@ import pandas as pd
 import multiprocessing as mp
 import hdbscan
 import itertools
+import threadpoolctl
 
 
 ###############################################################################                                                                                                                      [44/1010]
 ################################ - Functions - ################################
 
-def mp_cluster(df, n, gamma, ms, method='eom', metric='euclidean'):
+def mp_cluster(df, n, gamma, ms, method='eom', metric='euclidean', allow_single_cluster=False):
     """
     Asynchronous parallel function for use with hyperparameter_selection function
     """
@@ -56,7 +57,7 @@ def mp_cluster(df, n, gamma, ms, method='eom', metric='euclidean'):
                                 metric=metric,
                                 min_cluster_size=int(gamma),
                                 min_samples=ms,
-                                allow_single_cluster=False,
+                                allow_single_cluster=allow_single_cluster,
                                 core_dist_n_jobs=20).fit(df)
 
     min_cluster_size = clust_alg.min_cluster_size
@@ -67,7 +68,7 @@ def mp_cluster(df, n, gamma, ms, method='eom', metric='euclidean'):
     return (min_cluster_size, min_samples, validity_score, n_clusters)
 
 
-def hyperparameter_selection(df, cores=10, method='eom', metric='euclidean'):
+def hyperparameter_selection(df, cores=10, method='eom', metric='euclidean', allow_single_cluster=False):
     """
     Input:
     df - embeddings from UMAP
@@ -82,7 +83,7 @@ def hyperparameter_selection(df, cores=10, method='eom', metric='euclidean'):
     results = []
     n = df.shape[0]
     for gamma in range(2, int(np.log(n))):
-        mp_results = [pool.apply_async(mp_cluster, args=(df, n, gamma, ms, method, metric)) for ms in
+        mp_results = [pool.apply_async(mp_cluster, args=(df, n, gamma, ms, method, metric, allow_single_cluster)) for ms in
                       range(1, int(2 * np.log(n)))]
         for result in mp_results:
             result = result.get()
@@ -90,6 +91,8 @@ def hyperparameter_selection(df, cores=10, method='eom', metric='euclidean'):
             if result[2] >= .5:
                 logging.info(
                     f'min_cluster_size = {result[0]},  min_samples = {result[1]}, validity_score = {result[2]} n_clusters = {result[3]}')
+    pool.close()
+    pool.join()
 
     return results
 
@@ -111,24 +114,29 @@ def best_validity(source):
 
 # Calculates distances between clusters using minimum spanning trees
 def cluster_distances(embeddings, cluster_result, threads):
-    pool = mp.Pool(threads)
-    labels = set(cluster_result.labels_)
-    try:
-        labels.remove(-1)
-    except KeyError:
-        None
+    with threadpoolctl.threadpool_limits(limits=threads, user_api='blas'):
+        pool = mp.Pool(int(threads / 5))
+        labels = set(cluster_result.labels_)
+        logging.info(labels)
+        try:
+            labels.remove(-1)
+        except KeyError:
+            None
 
-    dist_mat = np.zeros((len(labels), len(labels)))
+        dist_mat = np.zeros((len(labels), len(labels)))
 
-    dist_results = [pool.apply_async(get_dist, args=(first, second, embeddings, cluster_result)) for (first, second) in
-                    itertools.combinations(labels, 2)]
+        dist_results = [pool.apply_async(get_dist, args=(first, second, embeddings, cluster_result)) for (first, second) in
+                        itertools.combinations(labels, 2)]
 
-    for result in dist_results:
-        result = result.get()
-        dist_mat[result[0], result[1]] = result[2]
-        dist_mat[result[1], result[0]] = result[2]
+        for result in dist_results:
+            result = result.get()
+            dist_mat[result[0], result[1]] = result[2]
+            dist_mat[result[1], result[0]] = result[2]
 
-    return dist_mat
+        pool.close()
+        pool.join()
+
+        return dist_mat
 
 
 def get_dist(first, second, embeddings, cluster_result):
@@ -142,8 +150,11 @@ def get_dist(first, second, embeddings, cluster_result):
     (second_nodes, second_edges) = hdbscan.validity.internal_minimum_spanning_tree(second_mr.astype(np.float64))
 
     # Calculates the density separation between two clusters using the above results
-    sep = hdbscan.validity.density_separation(embeddings, cluster_result.labels_, first, second, first_nodes,
-                                              second_nodes, first_core, second_core)
+    try:
+        sep = hdbscan.validity.density_separation(embeddings, cluster_result.labels_, first, second, first_nodes,
+                                                  second_nodes, first_core, second_core)
+    except ValueError:
+        sep = 1.0
 
     return first, second, sep
 