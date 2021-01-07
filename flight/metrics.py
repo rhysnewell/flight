a0a0d05713a7ae0ffc40a8dfcac126cac8752990
Rhys Newell <rhys.newell@hdr.qut.edu.au>
2021-01-07 13:21:18 +1000

    adding kl divergence metric

--- flight/metrics.py
+++ flight/metrics.py
@@ -57,80 +57,41 @@ def tnf(a, b, n_samples):
 
     # return euc_dist
 
-
 @numba.njit()
-def snv_corr(a, b, n_samples):
-    snv1 = a[n_samples:n_samples * 2]
-    snv2 = b[n_samples:n_samples * 2]
-    if len(set(snv1)) == 1 and len(set(snv2)) == 1:
-        return 1
-    else:
-        covariance_mat = np.cov(snv1, snv2, rowvar=True)
-        covariance = covariance_mat[0, 1]
-        var_a = covariance_mat[0, 0]
-        var_b = covariance_mat[1, 1]
-        vlr = -2 * covariance + var_a + var_b
-        rho = 1 - vlr / (var_a + var_b)
-        rho += 1
-        rho = 2 - rho
-        return rho
+def kl_divergence(a, b, n_samples):
+    """
+    a - The mean and variance vec for contig a over n_samples
+    b - The mean and variance vec for contig b over n_samples
 
-@numba.njit()
-def sv_corr(a, b, n_samples):
-    sv1 = a[n_samples*2:n_samples*3]
-    sv2 = b[n_samples*2:n_samples*3]
-    # check if all values where just 0 originally
-    if len(set(sv1)) == 1 and len(set(sv2)) == 1:
-        return 1
-    else:
-        covariance_mat = np.cov(sv1, sv2, rowvar=True)
-        covariance = covariance_mat[0, 1]
-        var_a = covariance_mat[0, 0]
-        var_b = covariance_mat[1, 1]
-        vlr = -2 * covariance + var_a + var_b
-        rho = 1 - vlr / (var_a + var_b)
-        rho += 1
-        rho = 2 - rho
-        return rho
+    returns the geometric mean of the KL divergences for contigs a and b over n_samples
+    """
+    a = iter(a)
+    b = iter(b)
+    # generate distirbutions for each sample
+    # and calculate divergence between them
+    # https://stats.stackexchange.com/questions/7440/kl-divergence-between-two-univariate-gaussians
+    kl_vec = []
+    for a_mean, b_mean in zip(a, b):
+        a_var = next(a)
+        b_var = next(b)
+        kl = np.log(np.sqrt(b_var) / np.sqrt(a_var)) + ((a_var + (a_mean - b_mean)**2) / (2*b_var)) - 1/2
+        kl_vec.append(kl)
 
-@numba.njit()
-def euclidean(a, b, n_samples):
-    # Since these compositonal arrays are CLR transformed
-    # This is the equivalent to the aitchinson distance but we calculat the l2 norm
-    euc_dist = np.linalg.norm(a[:n_samples] - b[:n_samples])
-
-    return euc_dist
-
-@numba.njit()
-def snv_euclidean(a, b, n_samples):
-    # Since these compositonal arrays are CLR transformed
-    # This is the equivalent to the aitchinson distance but we calculat the l2 norm
-    # check if all values where just 0 originally
-    snv1 = a[n_samples:n_samples * 2]
-    snv2 = b[n_samples:n_samples * 2]
-    if len(set(snv1)) == 1 and len(set(snv2)) == 1:
-        return 1
-    else:
-        euc_dist = np.linalg.norm(snv1 - snv2)
-        return euc_dist
-
-
-@numba.njit()
-def sv_euclidean(a, b, n_samples):
-    # Since these compositonal arrays are CLR transformed
-    # This is the equivalent to the aitchinson distance but we calculat the l2 norm
-    sv1 = a[n_samples * 2:n_samples * 3]
-    sv2 = b[n_samples * 2:n_samples * 3]
-    if len(set(sv1)) == 1 and len(set(sv2)) == 1:
-        return 1
-    else:
-        euc_dist = np.linalg.norm(sv1 - sv2)
-        return euc_dist
+    # convert to log space to avoid overflow errors
+    kl_vec = np.log(kl_vec)
+    # return the geometric mean
+    return np.exp(kl_vec.sum() / len(kl_vec))
 
 @numba.njit()
 def rho(a, b, n_samples):
-    # This is a transformed, inversed version of rho. Normal those -1 <= rho <= 1
-    # transformed rho: 0 <= rho <= 2, where 0 is perfect concordance
+    """
+    a - CLR transformed coverage distribution vector a
+    b - CLR transformed coverage distribution vector b
+
+    return - This is a transformed, inversed version of rho. Normal those -1 <= rho <= 1
+    transformed rho: 0 <= rho <= 2, where 0 is perfect concordance
+    """
+
     covariance_mat = np.cov(a[:n_samples], b[:n_samples], rowvar=True)
     covariance = covariance_mat[0, 1]
     var_a = covariance_mat[0, 0]
@@ -147,121 +108,19 @@ def rho(a, b, n_samples):
     
     return dist
 
-
-@numba.njit()
-def rho_tnf(a, b, n_samples):
-    # This is a transformed, inversed version of rho. Normal those -1 <= rho <= 1
-    # transformed rho: 0 <= rho <= 2, where 0 is perfect concordance
-    w = n_samples / (n_samples + 1) # weighting by number of samples same as in metabat2
-    l = min(a[0], b[0]) / (max(a[0], b[0]) + 1)
-    
-    covariance_mat = np.cov(a[1:], b[1:], rowvar=True)
-    covariance = covariance_mat[0, 1]
-    var_a = covariance_mat[0, 0]
-    var_b = covariance_mat[1, 1]
-    vlr = -2 * covariance + var_a + var_b
-    rho = 1 - vlr / (var_a + var_b)
-    rho += 1
-    rho = (2 - rho)
-    
-    return rho
-
-
-@numba.njit()
-def concordance(a, b, n_samples):
-    # This is a transformed, inversed version of rho. Normal those -1 <= rho <= 1
-    # transformed rho: 0 <= rho <= 2, where 0 is perfect concordance
-    covariance_mat = np.cov(a[:n_samples], b[:n_samples], rowvar=True)
-    covariance = covariance_mat[0, 1]
-    var_a = covariance_mat[0, 0]
-    var_b = covariance_mat[1, 1]
-    vlr = -2 * covariance + var_a + var_b
-    rho = 1 - vlr / (var_a + var_b)
-
-    return rho
-
-@numba.njit()
-def phi(a, b, n_samples):
-    covariance_mat = np.cov(a, b, rowvar=True)
-    covariance = covariance_mat[0, 1]
-    var_a = covariance_mat[0, 0]
-    var_b = covariance_mat[1, 1]
-    phi = 1 + (var_a / var_b) - 2 * np.sqrt(var_a / var_b) * covariance / np.sqrt(var_a * var_b)
-
-    return phi
-
-@numba.njit()
-def phi_dist(a, b, n_samples):
-    covariance_mat = np.cov(a, b, rowvar=True)
-    covariance = covariance_mat[0, 1]
-    var_a = covariance_mat[0, 0]
-    var_b = covariance_mat[1, 1]
-    phi_dist = abs(math.log(var_a / var_b)) + math.log(2) - math.log(covariance / np.sqrt(var_a * var_b) + 1)
-
-    return phi_dist
-
 @numba.njit()
 def aggregate_tnf(a, b, n_samples):
+    """
+    a, b - concatenated contig depth, variance, and TNF info with contig length at index 0
+    n_samples - the number of samples
+
+    returns - an aggregate distance metric between KL divergence and TNF
+    """
     w = n_samples / (n_samples + 1) # weighting by number of samples same as in metabat2
     l = min(a[0], b[0]) / (max(a[0], b[0]) + 1)
 
-    tnf_dist = tnf(a[1:], b[1:], n_samples)
-    if n_samples < 3:  
-        aitchinson = euclidean(a[1:], b[1:], n_samples)  
-        agg = (tnf_dist) * (aitchinson)
-    else:
-        rho_d = rho(a[1:], b[1:], n_samples)
-        agg = (tnf_dist) * rho_d
-    
-
-    return agg
-
-@numba.njit()
-def aggregate_variant_tnf(a, b, n_samples):
-    w = n_samples / (n_samples + 1) # weighting by number of samples same as in metabat2
-    # Need to weigh by differences in contig size. TNF becomes less reliable as contigs diverge in size
-    l = min(a[0], b[0]) / (max(a[0], b[0]) + 1)
-
-    tnf_dist = tnf(a[1:], b[1:], n_samples)
-    aitchinson = euclidean(a[1:], b[1:], n_samples)
-
-    if n_samples >= 3:
-        snv = snv_corr(a[1:], b[1:], n_samples)
-        sv = sv_corr(a[1:], b[1:], n_samples)
-        agg = (tnf_dist) * (aitchinson) * snv * sv
-        return agg
-    else:
-
-        # Weighted average of these metrics. Weighted by w
-        agg = (tnf_dist) * (aitchinson)
-
-        return agg
-
-@numba.njit()
-def tnf_dist(a, b, n_samples):
-    # w = n_samples / (n_samples + 1)  # weighting by number of samples same as in metabat2
-    # Need to weigh by differences in contig size. TNF becomes less reliable as contigs diverge in size
-    # l = min(a[0], b[0]) / (max(a[0], b[0]) + 1)
-
-    covariance_mat = np.cov(a[1+n_samples:], b[1+n_samples:], rowvar=True)
-    covariance = covariance_mat[0, 1]
-    var_a = covariance_mat[0, 0]
-    var_b = covariance_mat[1, 1]
-    vlr = -2 * covariance + var_a + var_b
-    rho = 1 - vlr / (var_a + var_b)
-    rho += 1
-    rho = 2 - rho
-
-    return rho
-
-
-
-@numba.njit()
-def aggregate(a, b, n_samples):
-    w = n_samples / (n_samples + 1) # weighting by number of samples same as in metabat2
-    # tnf_dist = tnf(a, b, n_samples)
-    aitchinson = euclidean(a, b, n_samples)
-    rho_val = rho(a, b, n_samples)
-    agg = aitchinson * rho_val
+    tnf_dist = tnf(a[1:], b[1:], n_samples*2)
+    kl = kl_divergence(a[1:n_samples*2 + 1], b[1:n_samples*2 + 1], n_samples)
+    agg = (tnf_dist**w) * kl**(1-w)
 
     return agg
